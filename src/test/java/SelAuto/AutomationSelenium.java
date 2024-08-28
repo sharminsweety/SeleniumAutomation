a08df0cccf7866dd7982a7b2bba84e318d99cdac
sharminsweety <sweetysharmin40@gmail.com>
2024-08-28 11:30:22 +0600

    first commit

---
+++ src/test/java/SelAuto/AutomationSelenium.java
@@ -0,0 +1,18 @@
+package SelAuto;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class AutomationSelenium {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.google.com/");
+
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
+
+        Thread.sleep(2000);
+        driver.quit();
+    }
+}
