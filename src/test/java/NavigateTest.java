6d411c21358954254f3b737bbb6912ac03513dd9
sharminsweety <sweetysharmin40@gmail.com>
2024-09-02 16:21:55 +0600

    navigate

--- src/test/java/NavigateTest.java
+++ src/test/java/NavigateTest.java
@@ -1,2 +1,15 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class NavigateTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://demo.guru99.com/test/newtours/register.php?authuser=0");
+        driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+        WebDriver.Navigation navigate = driver.navigate();
+        navigate.
+    }
 }
