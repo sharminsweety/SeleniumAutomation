0c1d8f79ebb720006e176e3c008288a22ecac9cb
sharminsweety <sweetysharmin40@gmail.com>
2024-09-04 10:41:15 +0600

    Alert

--- src/test/java/Class3/AlertTest.java
+++ src/test/java/Class3/AlertTest.java
@@ -1,4 +1,20 @@
 package Class3;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class AlertTest {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://demoqa.com/alerts");
+        driver.manage().window().maximize();
+
+        driver.findElement(By.id("alertButton")).click();
+        Thread.sleep(2000);
+        driver.switchTo().alert();
+
+
+    }
 }
